a810be52dde80d5a951f3ca9be5e9619c3cf3e4f
VictorArruda22 <victor.ti.henrique@gmail.com>
2024-12-19 14:23:47 -0300

    feat: adicionando repository do author com mapeamento de erros

--- internal/repository/author.go
+++ internal/repository/author.go
@@ -2,8 +2,12 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
+	"log"
 
 	"github.com/VictorArruda22/api-golang/internal/entities"
+	"github.com/VictorArruda22/api-golang/internal/utils"
+	"github.com/go-sql-driver/mysql"
 )
 
 type authorRepositoryImpl struct {
@@ -19,11 +23,11 @@ func (r *authorRepositoryImpl) Create(author entities.Author) ([]entities.Author
 	result, err := r.db.Exec("INSERT INTO Author (name, nascimento, nascionalidade) VALUES (?, ?, ?)",
 		author.Name, author.Nascimento, author.Nacionalidade)
 	if err != nil {
-		return nil, err
+		return nil, handlerError(err)
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		return nil, err
+		return nil, handlerError(err)
 	}
 	author.ID = int(id)
 	return []entities.Author{author}, nil
@@ -32,14 +36,16 @@ func (r *authorRepositoryImpl) Create(author entities.Author) ([]entities.Author
 // Delete implements entities.AuthorRepository.
 func (r *authorRepositoryImpl) Delete(id int) error {
 	_, err := r.db.Exec("DELETE FROM author WHERE id = ?", id)
-	return err
+	return handlerError(err)
 }
 
 // GetAll implements entities.AuthorRepository.
 func (r *authorRepositoryImpl) GetAll() ([]entities.Author, error) {
-	rows, err := r.db.Query("SELECT id, name, nascimento, nacionalidade FROM author")
+	log.Println("Repository: GetAll called")
+	rows, err := r.db.Query("SELECT Id, Name, BirthDate, Nationality FROM Author")
 	if err != nil {
-		return nil, err
+		log.Println("Repository: Error query %v", err)
+		return nil, handlerError(err)
 	}
 	defer rows.Close()
 
@@ -47,7 +53,8 @@ func (r *authorRepositoryImpl) GetAll() ([]entities.Author, error) {
 	for rows.Next() {
 		var author entities.Author
 		if err := rows.Scan(&author.ID, &author.Name, &author.Nascimento, &author.Nacionalidade); err != nil {
-			return nil, err
+			log.Println("Error scan")
+			return nil, handlerError(err)
 		}
 		authors = append(authors, author)
 	}
@@ -60,7 +67,7 @@ func (r *authorRepositoryImpl) GetByID(id int) ([]entities.Author, error) {
 	err := r.db.QueryRow("SELECT id, name, nascimento, nacionalidade FROM Author WHERE id = ?", id).
 		Scan(&author.ID, &author.Name, &author.Nascimento, &author.Nacionalidade)
 	if err != nil {
-		return []entities.Author{author}, err
+		return []entities.Author{author}, handlerError(err)
 	}
 	return []entities.Author{author}, nil
 }
@@ -70,4 +77,30 @@ func (r *authorRepositoryImpl) Update(author entities.Author) ([]entities.Author
 	_, err := r.db.Exec("UPDATE Author SET name = ?, nascimento = ?, nacionalidade = ? WHERE id = ?",
 		author.Name, author.Nascimento, author.Nacionalidade)
 	return nil, err
+}
+
+func handlerError(err error) error {
+	var mysqlErr *mysql.MySQLError
+	if errors.As(err, &mysqlErr) {
+		switch mysqlErr.Number {
+		case 1062:
+			if mysqlErr.Message == "Duplicate entry" {
+				return utils.ErrAuthorRepositoryInvalidID
+			}
+			err = utils.ErrAuthorRepositoryDuplicated
+		case 1452:
+			err = utils.ErrAuthorRepositoryForeignKey
+		case 1054:
+			err = utils.ErrAuthorRepositoryBadField
+		case 1048:
+			err = utils.ErrAuthorRepositoryNullValue
+		default:
+			return utils.ErrAuthorRepositoryInternalError
+		}
+	}
+	if errors.Is(err, sql.ErrNoRows) {
+		return utils.ErrAuthorRepositoryNotFound
+	}
+
+	return err
 }
