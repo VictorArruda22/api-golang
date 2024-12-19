61be719a05d639aa49289d0b0773c7a2df8f6469
VictorArruda22 <victor.ti.henrique@gmail.com>
2024-12-19 14:24:08 -0300

    feat: adicionando as rotas

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/VictorArruda22/api-golang/internal/handler"
 	"github.com/go-chi/chi/v5"
 )
 
-func RouterManagement() *chi.Mux {
+func RouterManagement(authorHandler handler.AuthorHandler) *chi.Mux {
 
 	router := chi.NewRouter()
 
 	router.Get("/", (func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	}))
+
+	router.Get("/authors", authorHandler.GetAllAuthors())
+	router.Get("/authors/{id}", authorHandler.GetByID())
+	router.Post("/authors", authorHandler.Create())
+	router.Put("/authors/{id}", authorHandler.Update())
+	router.Delete("/authors/{id}", authorHandler.Delete())
 
 	return router
 
