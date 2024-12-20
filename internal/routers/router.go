33dcfe448851c4f9293082a49b5fd6c5e5f3ac02
VictorArruda22 <victor.ti.henrique@gmail.com>
2024-12-20 17:05:09 -0300

    fix: corrigindo o nome do gerenciador de rotas

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -5,7 +5,7 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func RouterManagement(authorHandler handler.AuthorHandler) *chi.Mux {
+func RouterAuthorManagement(authorHandler handler.AuthorHandler) *chi.Mux {
 
 	router := chi.NewRouter()
 
